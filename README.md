f798a8aa458a2464558de8d2b98ccbf6c0f7986d
MAEKAWA Masahide <maekawa@daemon-systems.org>
2026-03-21 02:39:42 +0900

    Initial commot of the Minecraft resource pack ``Smooth Font''.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Minecraft Smooth Font
+
+The Minecraft resource pack ``Smooth Font'' , which replace with a smooth font.
+
+## Latest Version
+
+1.0.0
+
+## Smooth Fonts
+
+### モリサワ BIZ UD ゴシック (Morisawa BIZ UD Gothic)
+
+https://github.com/googlefonts/morisawa-biz-ud-gothic/
+
+### M+ Fonts
+
+https://mplusfonts.github.io/
+
+## For Java Edition (1.21.9/1.21.10/1.21.11 or later) users
+
+```
+% git clone https://github.com/cvsync/SmoothFont
+
+% cd SmoothFont
+
+% /bin/sh build.sh
+
+% ls 1.0.0/
+SmoothFont-bizud.zip   SmoothFont-mplus.zip
+```
---
+++ build.sh
@@ -0,0 +1,84 @@
+#! /bin/sh
+
+VER_MAJOR=1
+VER_MINOR=0
+VER_REV=0
+
+MAX_PACK_FORMAT=84
+MAX_PACK_FORMAT_MINOR=0
+
+MIN_PACK_FORMAT=69
+MIN_PACK_FORMAT_MINOR=0
+
+RESOURCE_PACK_NAME=SmoothFont
+
+TMPDIR=./tmp
+FONT_DSTDIR=${TMPDIR}/assets/minecraft/font
+
+font_pack() {
+	FONTNAME="$1"
+	FONTFILE_URL="$2"
+	FONTFILE=$(echo ${FONTFILE_URL##*/} | tr [A-Z] [a-z] | sed 's/-//g')
+
+	RESOURCE_PACK_FILE_JAVA="${RESOURCE_PACK_NAME}-${FONTNAME}".zip
+
+	#
+	# init
+	#
+	sudo rm -f -r ${TMPDIR}
+	mkdir -p ${TMPDIR} ${TMPDIR}/assets ${TMPDIR}/assets/minecraft
+	mkdir -p ${TMPDIR}/assets/minecraft/font ${TMPDIR}/assets/minecraft/font/fonts
+
+	# Download font file
+	curl -L ${FONTFILE_URL} -o ${FONT_DSTDIR}/fonts/${FONTFILE}
+
+	#
+	# pack.mcdata
+	#
+	_file=pack.mcmeta
+	cat "${RESOURCE_PACK_NAME}/${_file}.${FONTNAME}" | \
+	sed "s/XXXMAXPACKFORMATXXX/${MAX_PACK_FORMAT}/g" | \
+	sed "s/XXXMAXPACKFORMATMINORXXX/${MAX_PACK_FORMAT_MINOR}/g" | \
+	sed "s/XXXMINPACKFORMATXXX/${MIN_PACK_FORMAT}/g" | \
+	sed "s/XXXMINPACKFORMATMINORXXX/${MIN_PACK_FORMAT_MINOR}/g" | \
+	sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
+	sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
+	sed "s/XXXREVXXX/${VER_REV}/g" | \
+	sed "s///g" > "${TMPDIR}/${_file}"
+
+	#
+	# default.json / uniform.json
+	#
+	for _file in default.json uniform.json
+	do
+		cat ${RESOURCE_PACK_NAME}/assets/minecraft/font/${_file} | \
+		sed "s/XXXFONTFILEXXX/${FONTFILE}/g" | \
+		sed "s///g" > ${FONT_DSTDIR}/${_file}
+	done
+
+	#
+	# change owner / group
+	#
+	(cd ${TMPDIR}/ && sudo chown -R 0:0 .)
+
+	#
+	# zip-fy
+	#
+	(cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_JAVA} . -i "*")
+
+	#
+	# Clean up
+	#
+	sudo rm -f -r ${TMPDIR}
+}
+
+#
+# for Java Edition
+#
+sudo rm -f *.zip
+font_pack mplus https://github.com/coz-m/MPLUS_FONTS/raw/refs/heads/master/fonts/ttf/Mplus1-Medium.ttf
+font_pack bizud https://github.com/googlefonts/morisawa-biz-ud-gothic/raw/refs/heads/main/fonts/ttf/BIZUDPGothic-Regular.ttf
+
+mkdir -p ${VER_MAJOR}.${VER_MINOR}.${VER_REV}
+cp ./*.zip ${VER_MAJOR}.${VER_MINOR}.${VER_REV}/
+ls -l ${VER_MAJOR}.${VER_MINOR}.${VER_REV}/
